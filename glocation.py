2ba09b9cdc5251aa7043e34ca74f87694d8046a0
Zigelzi <40773205+Zigelzi@users.noreply.github.com>
2019-03-29 16:39:16 +0200

    First parsing attempt

--- glocation.py
+++ glocation.py
@@ -2,3 +2,17 @@ import json
 from datetime import datetime
 from collections import namedtuple
 
+Point = namedtuple('Point', 'latitude, longitude, datetime')
+
+def read_points():
+    with open('location_history.json') as f:
+        data = json.load(f)
+
+    for point in data['locations']:
+        yield Point(
+            point['latitudeE7'] / 10 ** 7,
+            point['longitudeE7'] / 10 ** 7,
+            datetime.fromtimestamp(int(point['timestampMs']) / 1000)
+        )
+
+points = read_points()
